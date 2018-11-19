dc0e1c434166b8c9c26c7fd5c3dce894cdb5869a
sfinlon <finlon@gmail.com>
2018-11-19 15:25:01 -0500

    update cif plugin to incorporate whitelists

--- minemeld/ft/cif.py
+++ minemeld/ft/cif.py
@@ -172,7 +172,7 @@ class Feed(basepoller.BasePollerFT):
 
         try:
             if len(ret) >= 1:
-                ret = f(ret, cols=cifsdk.constants.fields.split(','))
+                ret = f(ret, cols=cifsdk.constants.FIELDS.split(','))
                 return ujson.loads(ret)
 
             else:
