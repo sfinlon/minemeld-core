fdd0e0e82c9151c9136f03b46d9eb60a5c643fb8
sfinlon <finlon@gmail.com>
2018-11-20 11:35:48 -0500

    update cif plugin to incorporate whitelists

--- minemeld/ft/cif.py
+++ minemeld/ft/cif.py
@@ -154,7 +154,7 @@ class Feed(basepoller.BasePollerFT):
             timeout=900
         )
 
-        ret = cifclient.search(limit='25', filters=filters)
+        ret = cifclient.search(limit='25', filters=filters, format=json)
 
         wl_filters = copy.deepcopy(filters)
         wl_filters['tags'] = 'whitelist'
